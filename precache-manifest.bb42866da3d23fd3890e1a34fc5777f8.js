8b8549100b82d84653b8424bb22815355e620eeb
abhik-s-basu <abhik20165@iiitd.ac.in>
2024-02-06 23:25:25 +0530

    Updates

---
+++ precache-manifest.bb42866da3d23fd3890e1a34fc5777f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882a21b7d2a02d4cccaef901fdc7a53a",
+    "url": "/valinvitation/index.html"
+  },
+  {
+    "revision": "a1e516ab35ddb92bfdaf",
+    "url": "/valinvitation/static/css/main.f83dda73.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/valinvitation/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/valinvitation/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "a1e516ab35ddb92bfdaf",
+    "url": "/valinvitation/static/js/main.38d85148.chunk.js"
+  },
+  {
+    "revision": "3418e383c087d2d921de",
+    "url": "/valinvitation/static/js/runtime~main.64343aa2.js"
+  }
+]);
